dbb0345a1996ef843f1efb54b9d155e1a6eace22
DanielContrerasCh <danicontreras0409@gmail.com>
2025-07-14 18:45:50 -0600

    padding correction

--- src/views/pages/familyTree.jsx
+++ src/views/pages/familyTree.jsx
@@ -136,7 +136,7 @@ function FamilyTree() {
             {/* Family Tree Section */}
             <BoxBasic 
                 sx={{
-                    padding: '40px 20px',
+                    padding: '0px 20px',
                     minHeight: 'auto'
                 }}
             >
