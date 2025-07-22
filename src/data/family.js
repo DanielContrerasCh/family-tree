c361faf1109a63ef8b1e82fc8bc8a8b30a37f1b3
DanielContrerasCh <danicontreras0409@gmail.com>
2025-07-21 22:49:06 -0600

    more family members

--- src/data/family.js
+++ src/data/family.js
@@ -145,16 +145,17 @@ familyData.addRelationship('regina-mejia-mendoza', 'luz-maria-alvarez-mejia');
 familyData.addMember('manuel-blanco-cuevas', 'Manuel Blanco Cuevas', true, '/src/views/public/manuelBlancoCuevas.jpg', null, null);
 familyData.addSpouseRelationship('maria-del-refugio-alvarez-mejia', 'manuel-blanco-cuevas');
 
-familyData.addMember('apolinar-blanco-alvarez', 'Apolinar Blanco Alvarez', false, 'https://picsum.photos/150/150?random=1', null, null, 2); // 2
+familyData.addMember('apolinar-blanco-alvarez', 'Apolinar Blanco Alvarez', false, 'https://picsum.photos/150/150?random=1', null, null, 3); // 2
 familyData.addMember('gonzalo-blanco-alvarez', 'Gonzalo Blanco Álvarez', true, 'https://picsum.photos/150/150?random=1', null, null, 4); // 4
-familyData.addMember('benita-blanco-alvarez', 'Benita Blanco Álvarez', true, 'https://picsum.photos/150/150?random=1', null, null, 6); // 6
-familyData.addMember('maria-de-jesus-blanco-alvarez', 'María de Jesús Blanco Álvarez', true, 'https://picsum.photos/150/150?random=1', null, null, 5); // 5
-familyData.addMember('maria-del-carmen-blanco-alvarez', 'María del Carmen Blanco Álvarez', false, 'https://picsum.photos/150/150?random=1', null, null, 8); // 8
-familyData.addMember('francisca-blanco-alvarez', 'Francisca Blanco Álvarez', false, 'https://picsum.photos/150/150?random=1', null, null, 7); // 7
-familyData.addMember('cecilia-blanco-alvarez', 'Cecilia Blanco Álvarez', false, 'https://picsum.photos/150/150?random=1', null, null, 9); // 9
+familyData.addMember('benita-blanco-alvarez', 'Benita Blanco Álvarez', false, '/src/views/public/benitaBlancoAlvarez.jpg', null, null, 5); // 6
+familyData.addMember('maria-de-jesus-blanco-alvarez', 'María de Jesús Blanco Álvarez', true, '/src/views/public/chuyBlancoAlvarez.jpg', null, null, 6); // 5
+familyData.addMember('maria-del-carmen-blanco-alvarez', 'María del Carmen Blanco Álvarez', false, '/src/views/public/carmelitaBlancoAlvarez.jpg', null, null, 7); // 8
+familyData.addMember('francisca-blanco-alvarez', 'Francisca Blanco Álvarez', false, '/src/views/public/franciscaBlancoAlvarez.jpg', null, null, 8); // 7
+familyData.addMember('cecilia-blanco-alvarez', 'Cecilia Blanco Álvarez', false, '/src/views/public/ceciliaBlancoAlvarez.jpg', null, null, 9); // 9
 familyData.addMember('juan-blanco-alvarez', 'Juan Blanco Álvarez', false, 'https://picsum.photos/150/150?random=1', null, null, 10); // 10
-familyData.addMember('ruben-blanco-alvarez', 'Rubén Blanco Álvarez', false, 'https://picsum.photos/150/150?random=1', null, null, 1); // 1
-familyData.addMember('maria-luisa-blanco-alvarez', 'María Luisa Blanco Álvarez', false, 'https://picsum.photos/150/150?random=1', null, null, 3); // 3
+familyData.addMember('alma-rosa-blanco-alvarez', 'Alma Rosa Blanco Álvarez', true, 'https://picsum.photos/150/150?random=1', null, null, 11); // 10
+familyData.addMember('ruben-blanco-alvarez', 'Rubén Blanco Álvarez', true, 'https://picsum.photos/150/150?random=1', null, null, 2); // 1
+familyData.addMember('maria-luisa-blanco-alvarez', 'María Luisa Blanco Álvarez', true, 'https://picsum.photos/150/150?random=1', null, null, 1); // 3
 
 // Relaciones familiares Blanco Álvarez (hijos de María del Refugio y Manuel Blanco)
 familyData.addRelationship('maria-del-refugio-alvarez-mejia', 'apolinar-blanco-alvarez');
@@ -167,6 +168,7 @@ familyData.addRelationship('maria-del-refugio-alvarez-mejia', 'cecilia-blanco-al
 familyData.addRelationship('maria-del-refugio-alvarez-mejia', 'juan-blanco-alvarez');
 familyData.addRelationship('maria-del-refugio-alvarez-mejia', 'ruben-blanco-alvarez');
 familyData.addRelationship('maria-del-refugio-alvarez-mejia', 'maria-luisa-blanco-alvarez');
+familyData.addRelationship('maria-del-refugio-alvarez-mejia', 'alma-rosa-blanco-alvarez');
 
 familyData.addRelationship('manuel-blanco-cuevas', 'apolinar-blanco-alvarez');
 familyData.addRelationship('manuel-blanco-cuevas', 'gonzalo-blanco-alvarez');
@@ -178,6 +180,11 @@ familyData.addRelationship('manuel-blanco-cuevas', 'cecilia-blanco-alvarez');
 familyData.addRelationship('manuel-blanco-cuevas', 'juan-blanco-alvarez');
 familyData.addRelationship('manuel-blanco-cuevas', 'ruben-blanco-alvarez');
 familyData.addRelationship('manuel-blanco-cuevas', 'maria-luisa-blanco-alvarez');
+familyData.addRelationship('manuel-blanco-cuevas', 'alma-rosa-blanco-alvarez');
+
+// Familia de cecilia Blanco Álvarez
+familyData.addMember('alma-blanco-TODO', 'Alma Blanco TODO', false, '/src/views/public/almaBlancoTODO.jpg', null, null);
+familyData.addRelationship('cecilia-blanco-alvarez', 'alma-blanco-TODO');
 
 // Familia Castañeda Álvarez
 familyData.addMember('salvador-castañeda-espinosa', 'Salvador Castañeda Espinosa', true, 'https://picsum.photos/150/150?random=1', null, null);
@@ -243,6 +250,131 @@ familyData.addRelationship('jose-contreras-galarza', 'bertha-contreras-alvarez')
 familyData.addRelationship('jose-contreras-galarza', 'francisco-javier-contreras-alvarez');
 familyData.addRelationship('jose-contreras-galarza', 'jose-de-jesus-contreras-alvarez');
 
+// Hijos de Jose Luis Contreras Álvarez
+familyData.addMember('luis-ivan-contreras-ley', 'Luis Iván Contreras Ley', false, 'https://picsum.photos/150/150?random=1', null, null, 1); // 1
+familyData.addMember('julio-contreras-ley', 'Julio Contreras Ley', false, 'https://picsum.photos/150/150?random=1', null, null, 2); // 2
+familyData.addMember('eduardo-contreras-ley', 'Eduardo Contreras Ley', false, 'https://picsum.photos/150/150?random=1', null, null, 3); // 3
+
+// Relaciones familiares hijos de José Luis Contreras Álvarez
+familyData.addRelationship('jose-luis-contreras-alvarez', 'luis-ivan-contreras-ley');
+familyData.addRelationship('jose-luis-contreras-alvarez', 'julio-contreras-ley');
+familyData.addRelationship('jose-luis-contreras-alvarez', 'eduardo-contreras-ley');
+
+// Pareja de Ana María Contreras Álvarez
+familyData.addMember('marco-antonio-espinoza', 'Marco Antonio Espinoza', true, 'https://picsum.photos/150/150?random=1', null, null);
+familyData.addSpouseRelationship('ana-maria-contreras-alvarez', 'marco-antonio-espinoza');
+
+// Hijos de Ana María Contreras Álvarez
+familyData.addMember('guadalupe-espinoza-contreras', 'Guadalupe Espinoza Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addMember('ana-espinoza-contreras', 'Ana Espinoza Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 3);
+familyData.addMember('tere-espinoza-contreras', 'Tere Espinoza Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 2);
+familyData.addMember('angelita-espinoza-contreras', 'Angelita Espinoza Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 4);
+
+// Relaciones familiares hijos de Ana María Contreras Álvarez
+familyData.addRelationship('ana-maria-contreras-alvarez', 'guadalupe-espinoza-contreras');
+familyData.addRelationship('ana-maria-contreras-alvarez', 'ana-espinoza-contreras');
+familyData.addRelationship('ana-maria-contreras-alvarez', 'tere-espinoza-contreras');
+
+// Pareja de Francisco Javier Contreras Álvarez
+familyData.addMember('maria-del-pilar-garcia', 'María del Pilar García', false, '/src/views/public/piliGarcia.jpg', null, null);
+familyData.addSpouseRelationship('francisco-javier-contreras-alvarez', 'maria-del-pilar-garcia');
+
+// Hijos de Francisco Javier Contreras Álvarez
+familyData.addMember('panchito-contreras-garcia', 'Francisco Javier Contreras García', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addMember('jose-antonio-contreras-garcia', 'José Antonio Contreras García', false, 'https://picsum.photos/150/150?random=1', null, null, 2);
+familyData.addMember('eva-contreras-garcia', 'Eva Contreras García', false, 'https://picsum.photos/150/150?random=1', null, null, 3);
+familyData.addMember('jorge-luis-contreras-garcia', 'Jorge Luis Contreras García', false, 'https://picsum.photos/150/150?random=1', null, null, 4);
+familyData.addMember('miguel-angel-contreras-garcia', 'Miguel Ángel Contreras García', false, 'https://picsum.photos/150/150?random=1', null, null, 5);
+
+// Relaciones familiares hijos de Francisco Javier Contreras Álvarez
+familyData.addRelationship('francisco-javier-contreras-alvarez', 'panchito-contreras-garcia');
+familyData.addRelationship('francisco-javier-contreras-alvarez', 'jose-antonio-contreras-garcia');
+familyData.addRelationship('francisco-javier-contreras-alvarez', 'eva-contreras-garcia');
+familyData.addRelationship('francisco-javier-contreras-alvarez', 'jorge-luis-contreras-garcia');
+familyData.addRelationship('francisco-javier-contreras-alvarez', 'miguel-angel-contreras-garcia');
+
+familyData.addRelationship('maria-del-pilar-garcia', 'panchito-contreras-garcia');
+familyData.addRelationship('maria-del-pilar-garcia', 'jose-antonio-contreras-garcia');
+familyData.addRelationship('maria-del-pilar-garcia', 'eva-contreras-garcia');
+familyData.addRelationship('maria-del-pilar-garcia', 'jorge-luis-contreras-garcia');
+familyData.addRelationship('maria-del-pilar-garcia', 'miguel-angel-contreras-garcia');
+
+// Pareja de Bertha Contreras Álvarez
+familyData.addMember('leoncio-terrones-ortega', 'Leoncio Terrones Ortega', false, '/src/views/public/leonTerrones.jpg', null, null, 1);
+familyData.addSpouseRelationship('bertha-contreras-alvarez', 'leoncio-terrones-ortega');
+
+// Hijos de Bertha Contreras Álvarez
+familyData.addMember('jose-ramon-terrones-contreras', 'José Ramón Terrones Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addMember('alma-patricia-terrones-contreras', 'Alma Patricia Terrones Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 2);
+familyData.addMember('leon-sergio-terrones-contreras', 'Leon Sergio Terrones Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 3);
+familyData.addMember('jazmin-terrones-contreras', 'Jazmín Terrones Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 4);
+familyData.addMember('mario-alberto-terrones-contreras', 'Mario Alberto Terrones Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 5);
+
+// Relaciones familiares hijos de Bertha Contreras Álvarez
+familyData.addRelationship('bertha-contreras-alvarez', 'jose-ramon-terrones-contreras');
+familyData.addRelationship('bertha-contreras-alvarez', 'alma-patricia-terrones-contreras');
+familyData.addRelationship('bertha-contreras-alvarez', 'leon-sergio-terrones-contreras');
+familyData.addRelationship('bertha-contreras-alvarez', 'jazmin-terrones-contreras');
+familyData.addRelationship('bertha-contreras-alvarez', 'mario-alberto-terrones-contreras');
+
+// Pareja de Salvador Contreras Álvarez
+familyData.addMember('celia-galvan-perez', 'Celia Galván Perez', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addSpouseRelationship('salvador-contreras-alvarez', 'celia-galvan-perez');
+
+// Hijos de Salvador Contreras Álvarez
+familyData.addMember('mayra-contreras-rios', 'Mayra Contreras Ríos', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addMember('mireya-contreras-rios', 'Mireya Contreras Ríos', false, 'https://picsum.photos/150/150?random=1', null, null, 2);
+familyData.addMember('samantha-contreras-rios', 'Samantha Contreras Ríos', false, 'https://picsum.photos/150/150?random=1', null, null, 3);
+familyData.addMember('salvador-contreras-galvan', 'Salvador Contreras Galván', false, 'https://picsum.photos/150/150?random=1', null, null, 4);
+
+// Relaciones familiares hijos de Salvador Contreras Álvarez
+familyData.addRelationship('salvador-contreras-alvarez', 'mayra-contreras-rios');
+familyData.addRelationship('salvador-contreras-alvarez', 'mireya-contreras-rios');
+familyData.addRelationship('salvador-contreras-alvarez', 'samantha-contreras-rios');
+familyData.addRelationship('salvador-contreras-alvarez', 'salvador-contreras-galvan');
+
+familyData.addRelationship('celia-galvan-perez', 'salvador-contreras-galvan');
+
+// Pareja de Evangelina Contreras Álvarez
+familyData.addMember('carlos-romero-martinez', 'Carlos Romero Martínez', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addSpouseRelationship('evangelina-contreras-alvarez', 'carlos-romero-martinez');
+
+// Hijos de Evangelina Contreras Álvarez
+familyData.addMember('jennifer-romero-contreras', 'Jennifer Romero Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+
+// RElaciones familiares hijos de Evangelina Contreras Álvarez
+familyData.addRelationship('evangelina-contreras-alvarez', 'jennifer-romero-contreras');
+familyData.addRelationship('carlos-romero-martinez', 'jennifer-romero-contreras');
+
+// Pareja de Juan Carlos Contreras Álvarez
+familyData.addMember('monica-chavez-hernandez', 'Mónica Chávez Hernández', false, '/src/views/public/mama.jpg', null, null, 1);
+familyData.addSpouseRelationship('juan-carlos-contreras-alvarez', 'monica-chavez-hernandez');
+
+// Hijos de Juan Carlos Contreras Álvarez
+familyData.addMember('israel-contreras-chavez', 'Israel Contreras Chávez', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addMember('itzel-contreras-chavez', 'Itzel Contreras Chávez', false, 'https://picsum.photos/150/150?random=1', null, null, 2);
+familyData.addMember('daniel-contreras-chavez', 'Daniel Contreras Chávez', false, 'https://picsum.photos/150/150?random=1', null, null, 3);
+
+// Relaciones familiares hijos de Juan Carlos Contreras Álvarez
+familyData.addRelationship('juan-carlos-contreras-alvarez', 'israel-contreras-chavez');
+familyData.addRelationship('juan-carlos-contreras-alvarez', 'itzel-contreras-chavez');
+familyData.addRelationship('juan-carlos-contreras-alvarez', 'daniel-contreras-chavez');
+familyData.addRelationship('monica-chavez-hernandez', 'israel-contreras-chavez');
+familyData.addRelationship('monica-chavez-hernandez', 'itzel-contreras-chavez');
+familyData.addRelationship('monica-chavez-hernandez', 'daniel-contreras-chavez');
+
+// Hijos de María Martina Contreras Álvarez
+familyData.addMember('andrea-trevino-contreras', 'Andrea Treviño Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addMember('sofia-trevino-contreras', 'Sofía Treviño Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 2);
+familyData.addMember('erika-trevino-contreras', 'Erika Treviño Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 3);
+familyData.addMember('regina-trevino-contreras', 'Regina Treviño Contreras', false, 'https://picsum.photos/150/150?random=1', null, null, 4);
+
+// Relaciones familiares hijos de María Martina Contreras Álvarez
+familyData.addRelationship('maria-martina-contreras-alvarez', 'andrea-trevino-contreras');
+familyData.addRelationship('maria-martina-contreras-alvarez', 'sofia-trevino-contreras');
+familyData.addRelationship('maria-martina-contreras-alvarez', 'erika-trevino-contreras');
+familyData.addRelationship('maria-martina-contreras-alvarez', 'regina-trevino-contreras');
+
 // Familia Alvarez Ramos
 familyData.addMember('judith-ramos-parra', 'Judith Ramos Parra', true, '/src/views/public/judithRamosParra.jpg', null, null);
 familyData.addSpouseRelationship('salvador-alvarez-mejia', 'judith-ramos-parra');
@@ -291,6 +423,43 @@ familyData.addRelationship('rafael-garcia-delgado', 'rafael-garcia-alvarez');
 familyData.addRelationship('rafael-garcia-delgado', 'antonio-garcia-alvarez');
 familyData.addRelationship('rafael-garcia-delgado', 'ofelia-garcia-alvarez');
 familyData.addRelationship('rafael-garcia-delgado', 'rosa-maria-garcia-alvarez');
+
+// Pareja de antonio García Álvarez
+familyData.addMember('margarita-maldonado-villanueva', 'Margarita Maldonado Villanueva', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addSpouseRelationship('antonio-garcia-alvarez', 'margarita-maldonado-villanueva');
+
+// Hijos de Antonio García Álvarez
+familyData.addMember('cynthia-maria-garcia-maldonado', 'Cynthia María García Maldonado', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addMember('jose-antonio-garcia-maldonado', 'José Antonio García Maldonado', false, 'https://picsum.photos/150/150?random=1', null, null, 2);
+familyData.addMember('flor-margarita-garcia-maldonado', 'Flor Margarita García Maldonado', false, 'https://picsum.photos/150/150?random=1', null, null, 3);
+familyData.addMember('rafael-manuel-garcia-maldonado', 'Rafael Manuel García Maldonado', false, 'https://picsum.photos/150/150?random=1', null, null, 4);
+
+// Relaciones familiares hijos de Antonio García Álvarez
+familyData.addRelationship('antonio-garcia-alvarez', 'cynthia-maria-garcia-maldonado');
+familyData.addRelationship('antonio-garcia-alvarez', 'jose-antonio-garcia-maldonado');
+familyData.addRelationship('antonio-garcia-alvarez', 'flor-margarita-garcia-maldonado');
+familyData.addRelationship('antonio-garcia-alvarez', 'rafael-manuel-garcia-maldonado');
+familyData.addRelationship('margarita-maldonado-villanueva', 'cynthia-maria-garcia-maldonado');
+familyData.addRelationship('margarita-maldonado-villanueva', 'jose-antonio-garcia-maldonado');
+familyData.addRelationship('margarita-maldonado-villanueva', 'flor-margarita-garcia-maldonado');
+familyData.addRelationship('margarita-maldonado-villanueva', 'rafael-manuel-garcia-maldonado');
+
+// Pareja de Rosa María García Álvarez
+familyData.addMember('cecilio-sanchez', 'Cecilio Sánchez', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addSpouseRelationship('rosa-maria-garcia-alvarez', 'cecilio-sanchez');
+
+// Hijos de Rosa María García Álvarez
+familyData.addMember('cecilia-sanchez-garcia', 'Cecilia Sanchez García', false, 'https://picsum.photos/150/150?random=1', null, null, 1);
+familyData.addMember('rosa-sanchez-garcia', 'Rosa Sanchez García', false, 'https://picsum.photos/150/150?random=1', null, null, 2);
+familyData.addMember('luis-sanchez-garcia', 'Luis Sanchez García', false, 'https://picsum.photos/150/150?random=1', null, null, 3);
+
+// Relaciones familiares hijos de Rosa María García Álvarez
+familyData.addRelationship('rosa-maria-garcia-alvarez', 'cecilia-sanchez-garcia');
+familyData.addRelationship('rosa-maria-garcia-alvarez', 'rosa-sanchez-garcia');
+familyData.addRelationship('rosa-maria-garcia-alvarez', 'luis-sanchez-garcia');
+familyData.addRelationship('cecilio-sanchez', 'cecilia-sanchez-garcia');
+familyData.addRelationship('cecilio-sanchez', 'rosa-sanchez-garcia');
+familyData.addRelationship('cecilio-sanchez', 'luis-sanchez-garcia');
 
 // Familia Álvarez Cejudo
 familyData.addMember('yolanda-cejudo-uribe', 'Yolanda Cejudo Uribe', false, '/src/views/public/yolandaCejudoUribe.jpg', null, null);
